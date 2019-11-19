c6f5a611d5c577f548d184ada46693aabe149a49
CelinaPeralta <celper19@bergen.org>
2019-11-18 20:34:08 -0500

    1234

---
+++ 1234_nov18_3.py
@@ -0,0 +1,16 @@
+#1234 - nov 18 no. 3
+
+Clock.bpm = 160
+
+d1 >> play("v v(v[*v])")
+d2 >> play("--[---]-").stop()
+d3 >> play("x o ").every(4,"stutter")
+d4 >> play("1234", rate=1, drive=0.05, sample=2, dur=PDur(5, 16), amp=PZ12([0, 1], [1, 0.5])[:15], lpf=1000).every(3, "reverse")
+
+var.prog = var([2, -1], dur=8)
+
+b1 >> jbass(var.prog, dur=1/2, oct=var([4.5, 4.75], [16,16]))
+
+
+
+
